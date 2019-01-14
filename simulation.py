f679c14101273e692a9e34c35ab3be0ece3dfa89
HugoooPerrin <b00605838@essec.edu>
2019-01-14 11:49:04 +0100

    update

--- simulation.py
+++ simulation.py
@@ -22,7 +22,7 @@ from VL_BFGS import *
 #================================ 1. DATA
 
 
-def simulate():
+def simulate(n_features, n_samples):
 
     n = Normal(0, 1)
     coefs = n.sample(torch.Size([n_features]))
@@ -33,101 +33,103 @@ def simulate():
 #=========================================================================================================
 #================================ 2. OPTIMIZATION
 
-
 # Parameters
-n_features = 8000
-n_samples = 100000
+n_features = [1000, 2000, 4000, 8000]
+n_samples = [100000, 100000, 100000, 100000]
 
 lbda = 0.1
 
+for n_feature, n_sample in zip(n_features, n_samples):
 
-# Initialization
-w0 = torch.zeros(n_features)
-f = logistic_loss
-f_grad = logistic_grad
-
-
-# CPU
-def cpu(X, y, w0, lbda):
-    optimizer = lbfgs(f, f_grad, m=10, vector_free=False, device='cpu')
-    _, _, cpu_time_comp, cpu_time_com = optimizer.fit(X, y, w0, lbda)
-
-    return cpu_time_comp, cpu_time_com
+    # Initialization
+    w0 = torch.zeros(n_feature)
+    f = logistic_loss
+    f_grad = logistic_grad
 
 
-# GPU
-def gpu(X, y, w0, lbda):
-    optimizer = lbfgs(f, f_grad, m=10, vector_free=False, device='cuda:0')
-    _, _, gpu_time_comp, gpu_time_com = optimizer.fit(X, y, w0, lbda)
+    # CPU
+    def cpu(X, y, w0, lbda):
+        optimizer = lbfgs(f, f_grad, m=10, vector_free=False, device='cpu')
+        _, _, cpu_time_comp, cpu_time_com = optimizer.fit(X, y, w0, lbda)
 
-    return gpu_time_comp, gpu_time_com
-
-
-# GPU (vector free)
-def gpu_VL(X, y, w0, lbda):
-    optimizer = lbfgs(f, f_grad, m=10, vector_free=True, device='cuda:0')
-    _, _, gpu_vl_time_comp, gpu_vl_time_com = optimizer.fit(X, y, w0, lbda)
-
-    return gpu_vl_time_comp, gpu_vl_time_com
+        return cpu_time_comp, cpu_time_com
 
 
-# Test
-X, y = simulate()
-t1, t2 = gpu(X, y, w0, lbda)
+    # GPU
+    def gpu(X, y, w0, lbda):
+        optimizer = lbfgs(f, f_grad, m=10, vector_free=False, device='cuda:0')
+        _, _, gpu_time_comp, gpu_time_com = optimizer.fit(X, y, w0, lbda)
+
+        return gpu_time_comp, gpu_time_com
 
 
-print('>> Training on %2d samples and %2d features' % X.size())
+    # GPU (vector free)
+    def gpu_VL(X, y, w0, lbda):
+        optimizer = lbfgs(f, f_grad, m=10, vector_free=True, device='cuda:0')
+        _, _, gpu_vl_time_comp, gpu_vl_time_com = optimizer.fit(X, y, w0, lbda)
+
+        return gpu_vl_time_comp, gpu_vl_time_com
 
 
-##============================
-## Monitoring time execution
-
-REPEAT = 20
-SHUFFLE = False
-
-#==============
-# CPU
-cpu_time_comp = []
-cpu_time_com = []
-for _ in range(REPEAT):
-
-    if SHUFFLE:
-        X, y = simulate()
-    t1, t2 = cpu(X, y, w0, lbda)
-    cpu_time_comp.append(t1)
-    cpu_time_com.append(t2)
-
-print('\n>> CPU computing time: %.2fs +-%.2fs' % (np.mean(cpu_time_comp), np.std(cpu_time_comp)))
-print('>> CPU communication time: %.2fs +-%.2fs\n' % (np.mean(cpu_time_com), np.std(cpu_time_com)))
-
-
-#==============
-# GPU
-gpu_time_comp = []
-gpu_time_com = []
-for _ in range(REPEAT):
-
-    if SHUFFLE:
-        X, y = simulate()
+    # Test
+    X, y = simulate(n_feature, n_sample)
     t1, t2 = gpu(X, y, w0, lbda)
-    gpu_time_comp.append(t1)
-    gpu_time_com.append(t2)
-
-print('>> GPU computing time: %.2fs +-%.2fs' % (np.mean(gpu_time_comp), np.std(gpu_time_comp)))
-print('>> GPU communication time: %.2fs +-%.2fs\n' % (np.mean(gpu_time_com), np.std(gpu_time_com)))
 
 
-#==============
-# GPU (vector free)
-gpu_vl_time_comp = []
-gpu_vl_time_com = []
-for _ in range(REPEAT):
+    print('>> Training on %2d samples and %2d features' % X.size())
 
-    if SHUFFLE:
-        X, y = simulate()
-    t1, t2 = gpu_VL(X, y, w0, lbda)
-    gpu_vl_time_comp.append(t1)
-    gpu_vl_time_com.append(t2)
 
-print('>> GPU (vector free) computing time: %.2fs +-%.2fs' % (np.mean(gpu_vl_time_comp), np.std(gpu_vl_time_comp)))
-print('>> GPU (vector free) communicating time: %.2fs +-%.2fs\n' % (np.mean(gpu_vl_time_com), np.std(gpu_vl_time_com)))
+    ##============================
+    ## Monitoring time execution
+
+    REPEAT = 10
+    SHUFFLE = True
+
+    #==============
+    # CPU
+    cpu_time_comp = []
+    cpu_time_com = []
+    for _ in range(REPEAT):
+
+        if SHUFFLE:
+            X, y = simulate(n_feature, n_sample)
+        t1, t2 = cpu(X, y, w0, lbda)
+        cpu_time_comp.append(t1)
+        cpu_time_com.append(t2)
+
+    print('\n   CPU computing time: %.2fs +-%.2fs' % (np.mean(cpu_time_comp), np.std(cpu_time_comp)))
+    print('   CPU communication time: %.2fs +-%.2fs\n' % (np.mean(cpu_time_com), np.std(cpu_time_com)))
+
+
+    #==============
+    # GPU
+    gpu_time_comp = []
+    gpu_time_com = []
+    for _ in range(REPEAT):
+
+        if SHUFFLE:
+            X, y = simulate(n_feature, n_sample)
+        t1, t2 = gpu(X, y, w0, lbda)
+        gpu_time_comp.append(t1)
+        gpu_time_com.append(t2)
+
+    print('   GPU computing time: %.2fs +-%.2fs' % (np.mean(gpu_time_comp), np.std(gpu_time_comp)))
+    print('   GPU communication time: %.2fs +-%.2fs\n' % (np.mean(gpu_time_com), np.std(gpu_time_com)))
+
+
+    #==============
+    # GPU (vector free)
+    gpu_vl_time_comp = []
+    gpu_vl_time_com = []
+    for _ in range(REPEAT):
+
+        if SHUFFLE:
+            X, y = simulate(n_feature, n_sample)
+        t1, t2 = gpu_VL(X, y, w0, lbda)
+        gpu_vl_time_comp.append(t1)
+        gpu_vl_time_com.append(t2)
+
+    print('   GPU (vector free) computing time: %.2fs +-%.2fs' % (np.mean(gpu_vl_time_comp), np.std(gpu_vl_time_comp)))
+    print('   GPU (vector free) communicating time: %.2fs +-%.2fs\n' % (np.mean(gpu_vl_time_com), np.std(gpu_vl_time_com)))
+
+    print('-------------------------------------------------\n')
